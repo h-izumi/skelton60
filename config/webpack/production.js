839f22fbb6b892d662b7c75033fcfb6c034cf5b4
Hayato IZUMI <h-izumi@9uelle.jp>
2019-04-05 16:28:02 +0900

    Webpacker - Do not output sourceMap in produciton.

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,42 @@
+// based on webpacker/package/environments/production.js v4.0.2
+
+const TerserPlugin = require('terser-webpack-plugin')
+
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
+
+environment.config.merge({
+  devtool: 'none',
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        cache: true,
+        sourceMap: false,
+        terserOptions: {
+          parse: {
+            // Let terser parse ecma 8 code but always output
+            // ES5 compliant code for older browsers
+            ecma: 8
+          },
+          compress: {
+            ecma: 5,
+            warnings: false,
+            comparisons: false
+          },
+          mangle: {
+            safari10: true
+          },
+          output: {
+            ecma: 5,
+            comments: false,
+            ascii_only: true
+          }
+        }
+      })
+    ]
+  }
+})
 
 module.exports = environment.toWebpackConfig()
